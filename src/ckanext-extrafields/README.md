03cfdee3b5d8c646f9cf346013f6120497ca0490
root <root@PT10851-ROTH.rothamsted.ac.uk>
2024-02-16 09:49:35 +0000

    first commit

---
+++ src/ckanext-extrafields/README.md
@@ -0,0 +1,25 @@
+[![Tests](https://github.com//ckanext-extrafields/workflows/Tests/badge.svg?branch=main)](https://github.com//ckanext-extrafields/actions)
+
+# ckanext-extrafields
+
+A custom extension to add multiple metadata fields suggested by DataCite 4.5
+Added metadata fields are:
+        # DOI
+        # Creator
+        # Publisher
+        # Publication Year
+        # Resource Type
+        # Geo Location Longitude
+        # Geo Location Latitude
+        # Geo Location Place
+## Requirements
+
+CKAN developer setup
+Python Venv
+
+
+## Installation
+
+python setup.py develop  //on src/ckanext-extrafields
+
+
---
+++ src/ckanext-extrafields/ckanext/extrafields/plugin.py
@@ -0,0 +1,195 @@
+# encoding: utf-8
+from __future__ import annotations
+
+from ckan.types import Schema
+import ckan.plugins as p
+from ckan.plugins.toolkit import Invalid
+import ckan.plugins.toolkit as tk
+
+
+class ExampleIDatasetFormPlugin(tk.DefaultDatasetForm, p.SingletonPlugin):
+    p.implements(p.IDatasetForm)
+    p.implements(p.IConfigurer)
+
+    def create_package_schema(self) -> Schema:
+        # let's grab the default schema in our plugin
+        schema: Schema = super(
+            ExampleIDatasetFormPlugin, self).create_package_schema()
+        # our custom field
+        schema.update({
+
+            'given_name': [tk.get_validator('ignore_missing'),
+                            tk.get_converter('convert_to_extras')],
+            'family_name': [tk.get_validator('ignore_missing'),
+                            tk.get_converter('convert_to_extras')],
+            'creator': [tk.get_validator('not_empty'),
+                            tk.get_converter('convert_to_extras')],
+            'nameIdentifier': [tk.get_validator('not_empty'),
+                            tk.get_converter('convert_to_extras')],
+            'publisher': [tk.get_validator('not_empty'),
+                            tk.get_converter('convert_to_extras')],    
+            'publicationYear': [tk.get_validator('not_empty'), tk.get_validator('year_validator'),
+                                 tk.get_converter('convert_to_extras')], 
+            'abstract': [tk.get_validator('not_empty'),
+                                 tk.get_converter('convert_to_extras')],
+            'methods': [tk.get_validator('not_empty'),
+                                 tk.get_converter('convert_to_extras')],
+            'technicalInfo': [tk.get_validator('ignore_missing'),
+                                 tk.get_converter('convert_to_extras')],
+            'experimentCode': [tk.get_validator('not_empty'),
+                            tk.get_converter('convert_to_extras')],
+            'Codes': [tk.get_validator('ignore_missing'),
+                            tk.get_converter('convert_to_extras')],            
+            'resourceTypeGeneral': [tk.get_validator('not_empty'),
+                                 tk.get_converter('convert_to_extras')],
+            'resourceType': [tk.get_validator('not_empty'),
+                                 tk.get_converter('convert_to_extras')],
+            'language': [tk.get_validator('ignore_missing'),
+                                 tk.get_converter('convert_to_extras')],
+            'funderName': [tk.get_validator('not_empty'),
+                                 tk.get_converter('convert_to_extras')],
+            'funderIdentifier': [tk.get_validator('not_empty'),
+                                 tk.get_converter('convert_to_extras')],
+            'contributorDetails': [tk.get_validator('ignore_missing'),
+                                 tk.get_converter('convert_to_extras')]
+                      
+        })        
+        return schema
+    def update_package_schema(self) -> Schema:
+        schema: Schema = super(
+            ExampleIDatasetFormPlugin, self).update_package_schema()
+        # our custom field
+        schema.update({
+
+            'given_name': [tk.get_validator('ignore_missing'),
+                            tk.get_converter('convert_to_extras')],
+            'family_name': [tk.get_validator('ignore_missing'),
+                            tk.get_converter('convert_to_extras')],
+            'creator': [tk.get_validator('not_empty'),
+                            tk.get_converter('convert_to_extras')],
+            'nameIdentifier': [tk.get_validator('not_empty'),
+                            tk.get_converter('convert_to_extras')],
+            'publisher': [tk.get_validator('not_empty'),
+                            tk.get_converter('convert_to_extras')],                            
+            'publicationYear': [tk.get_validator('not_empty'), tk.get_validator('year_validator'),
+                                 tk.get_converter('convert_to_extras')], 
+            'abstract': [tk.get_validator('not_empty'),
+                                 tk.get_converter('convert_to_extras')],
+            'methods': [tk.get_validator('not_empty'),
+                                 tk.get_converter('convert_to_extras')],
+            'technicalInfo': [tk.get_validator('ignore_missing'),
+                                 tk.get_converter('convert_to_extras')],
+            'experimentCode': [tk.get_validator('ignore_missing'),
+                            tk.get_converter('convert_to_extras')],
+            'Codes': [tk.get_validator('ignore_missing'),
+                            tk.get_converter('convert_to_extras')],
+            'resourceTypeGeneral': [tk.get_validator('not_empty'),
+                                 tk.get_converter('convert_to_extras')],
+            'resourceType': [tk.get_validator('not_empty'),
+                                 tk.get_converter('convert_to_extras')],
+            'language': [tk.get_validator('ignore_missing'),
+                                 tk.get_converter('convert_to_extras')],
+            'funderName': [tk.get_validator('not_empty'),
+                                 tk.get_converter('convert_to_extras')],   
+            'funderIdentifier': [tk.get_validator('not_empty'),
+                                 tk.get_converter('convert_to_extras')],   
+            'contributorDetails': [tk.get_validator('ignore_missing'),
+                                 tk.get_converter('convert_to_extras')]                          
+        })       
+        return schema
+    def show_package_schema(self) -> Schema:
+        schema: Schema = super(
+            ExampleIDatasetFormPlugin, self).show_package_schema()
+        schema.update({
+
+            'given_name': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')],
+            'family_name': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')],
+            'creator': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')],
+            'nameIdentifier': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')],
+            'publisher': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')],
+            'publicationYear': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'abstract': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'methods': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'technicalInfo': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'experimentCode': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')],
+            'Codes': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')],
+            'resourceTypeGeneral': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'resourceType': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'language': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'funderName': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'funderIdentifier': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'contributorDetails': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')]
+        })       
+        return schema
+
+        schema: Schema = super(
+            ExampleIDatasetFormPlugin, self).show_package_schema()
+        schema.update({
+
+            'given_name': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')],
+            'family_name': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')],
+            'creator': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')],
+            'nameIdentifier': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')],
+            'publisher': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')],
+            'publicationYear': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'abstract': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'methods': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'technicalInfo': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'experimentCode': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')],
+            'Codes': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')],
+            'resourceTypeGeneral': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'resourceType': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'language': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'funderName': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'funderIdentifier': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')],
+            'contributorDetails': [tk.get_converter('convert_from_extras'),
+                                 tk.get_validator('ignore_missing')]                       
+        })       
+        return schema
+
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return True
+
+    def package_types(self) -> list[str]:
+        # This plugin doesn't handle any special package types, it just
+        # registers itself as the default (above).
+        return []
+    def update_config(self, config: CKANConfig):
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        tk.add_template_directory(config, 'templates')        
