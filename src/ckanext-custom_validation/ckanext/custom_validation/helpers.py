d0128d405918ec39f6b3c1ead4c082de5f5e204b
GokulVijayakumarRam <gokul.vijayakumar-ram@rothamsted.ac.uk>
2024-11-21 11:11:03 +0000

    ok

--- src/ckanext-custom_validation/ckanext/custom_validation/helpers.py
+++
@@ -1,9 +0,0 @@
-
-def custom_validation_hello():
-    return "Hello, custom_validation!"
-
-
-def get_helpers():
-    return {
-        "custom_validation_hello": custom_validation_hello,
-    }
--- src/ckanext-custom_validation/ckanext/custom_validation/tests/test_helpers.py
+++
@@ -1,7 +0,0 @@
-"""Tests for helpers.py."""
-
-import ckanext.custom_validation.helpers as helpers
-
-
-def test_custom_validation_hello():
-    assert helpers.custom_validation_hello() == "Hello, custom_validation!"
